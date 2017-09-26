524a91b02df3c6036be4b71921da0d2042cf0db4
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2017-09-26 11:55:53 -0700

    moving files and fixing gitignore

--- custom.js
+++
@@ -1,8 +0,0 @@
-// Configure CodeMirror
-require([
-    'nbextensions/vim_binding',   // depends your installation
-], function() {
-    // Map jj to <Esc>
-    CodeMirror.Vim.map("fd", "<Esc>", "insert"); // Use fd to escape.
-    // CodeMirror.Vim.map("fd", "<Esc>", "insert");
-});
